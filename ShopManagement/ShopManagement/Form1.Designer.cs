5c09f0697379bcb3adcab916cf8fcb0ffefe7197
AlauddinMunshi <AlauddinMunshi@users.noreply.github.com>
2019-08-17 01:18:32 +0600

    hh
    hh

--- ShopManagement/ShopManagement/Form1.Designer.cs
+++ ShopManagement/ShopManagement/Form1.Designer.cs
@@ -50,7 +50,6 @@
             // 
             // MenuPanel
             // 
-            this.MenuPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.MenuPanel.Controls.Add(this.label1);
             this.MenuPanel.Controls.Add(this.button2);
             this.MenuPanel.Controls.Add(this.button1);
@@ -69,7 +68,7 @@
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(231, 31);
+            this.label1.Size = new System.Drawing.Size(231, 33);
             this.label1.TabIndex = 2;
             this.label1.Text = "Shop Management";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -82,10 +81,10 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(966, 0);
+            this.button2.Location = new System.Drawing.Point(968, 0);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(51, 31);
+            this.button2.Size = new System.Drawing.Size(51, 33);
             this.button2.TabIndex = 1;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
@@ -98,10 +97,10 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(1017, 0);
+            this.button1.Location = new System.Drawing.Point(1019, 0);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(48, 31);
+            this.button1.Size = new System.Drawing.Size(48, 33);
             this.button1.TabIndex = 0;
             this.button1.Text = "X";
             this.button1.UseVisualStyleBackColor = true;
